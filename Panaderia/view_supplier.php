7ee7e08ec918198e95108ff6e04bb9870bb1203b
Dalbvas <52789866+Dalbvas@users.noreply.github.com>
2019-10-17 07:23:51 -0600

    Update view_supplier.php

--- Panaderia/view_supplier.php
+++ Panaderia/view_supplier.php
@@ -6,7 +6,7 @@ include_once("init.php");
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>POSNIC - Add supplier</title>
+        <title>Panaderia</title>
 
         <!-- Stylesheets -->
         <!---->

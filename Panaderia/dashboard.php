cf838c74164b774b4b5127cfe0025def8e03328b
Dalbvas <52789866+Dalbvas@users.noreply.github.com>
2019-10-16 22:28:35 -0600

    Update dashboard.php

--- Panaderia/dashboard.php
+++ Panaderia/dashboard.php
@@ -33,14 +33,14 @@ include_once("init.php");
     <div class="page-full-width cf">
 
         <ul id="tabs" class="fl">
-            <li><a href="dashboard.php" class="active-tab dashboard-tab">Dashboard</a></li>
-            <li><a href="view_sales.php" class="sales-tab">Sales</a></li>
-            <li><a href="view_customers.php" class=" customers-tab">Customers</a></li>
-            <li><a href="view_purchase.php" class="purchase-tab">Purchase</a></li>
-            <li><a href="view_supplier.php" class=" supplier-tab">Supplier</a></li>
-            <li><a href="view_product.php" class=" stock-tab">Stocks / Products</a></li>
-            <li><a href="view_payments.php" class="payment-tab">Payments / Outstandings</a></li>
-            <li><a href="view_report.php" class="report-tab">Reports</a></li>
+            <li><a href="dashboard.php" class="active-tab dashboard-tab">Tablero</a></li>
+            <li><a href="view_sales.php" class="sales-tab">Ventas</a></li>
+            <li><a href="view_customers.php" class=" customers-tab">Clientes</a></li>
+            <li><a href="view_purchase.php" class=" purchase-tab">Compras</a></li>
+            <li><a href="view_supplier.php" class=" supplier-tab">Proveedores</a></li>
+            <li><a href="view_product.php" class=" stock-tab">Stocks / Productos</a></li>
+            <li><a href="view_payments.php" class="payment-tab">Pagos</a></li>
+            <li><a href="view_report.php" class="report-tab">Reportes</a></li>
         </ul>
         <!-- end tabs -->
 

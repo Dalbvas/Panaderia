7e6cbb69722b72f3b689deb35cbaee559396ea5b
mapeyal <53226636+mapeyal@users.noreply.github.com>
2019-10-17 07:38:44 -0600

    Update view_product.php

--- Panaderia/view_product.php
+++ Panaderia/view_product.php
@@ -6,7 +6,7 @@ include_once("init.php");
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Señor Pan</title>
+        <title>Señor Pan - Productos</title>
 
         <!-- Stylesheets -->
         <!---->

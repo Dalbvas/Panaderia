815cd96d38ae253cb4ac9b41d8c8a038b1a1bf4d
shgen24 <52790994+shgen24@users.noreply.github.com>
2019-10-17 09:55:23 -0600

    Update view_customers.php

--- Panaderia/view_customers.php
+++ Panaderia/view_customers.php
@@ -86,9 +86,9 @@ include_once("init.php");
 
                 <div class="content-module-heading cf">
 
-                    <h3 class="fl">Customers</h3>
-                    <span class="fr expand-collapse-text">Click to collapse</span>
-                    <span class="fr expand-collapse-text initial-expand">Click to expand</span>
+                    <h3 class="fl">Clientes</h3>
+                    <span class="fr expand-collapse-text">Click para minimizar</span>
+                    <span class="fr expand-collapse-text initial-expand">Click para expandir</span>
 
                 </div>
                 <!-- end content-module-heading -->
@@ -98,16 +98,16 @@ include_once("init.php");
 
                     <table>
                         <form action="" method="post" name="search">
-                            <input name="searchtxt" type="text" class="round my_text_box" placeholder="Search">
+                            <input name="searchtxt" type="text" class="round my_text_box" placeholder="Buscar">
                             &nbsp;&nbsp;<input name="Search" type="submit" class="my_button round blue   text-upper"
-                                               value="Search">
+                                               value="Buscar">
                         </form>
                         <form action="" method="get" name="limit_go">
-                            Page per Record<input name="limit" type="text" class="round my_text_box" id="search_limit"
+                            Pagina por registro<input name="limit" type="text" class="round my_text_box" id="search_limit"
                                                   style="margin-left:5px;"
                                                   value="<?php if (isset($_GET['limit'])) echo $_GET['limit']; else echo "10"; ?>"
                                                   size="3" maxlength="3">
-                            <input name="go" type="button" value="Go" class=" round blue my_button  text-upper"
+                            <input name="go" type="button" value="IR" class=" round blue my_button  text-upper"
                                    onclick="return confirmLimitSubmit()">
                         </form>
 
@@ -121,7 +121,7 @@ include_once("init.php");
                             <input type="button" name="unselectall" value="DeSelectAll"
                                    class="my_button round blue   text-upper" onClick="uncheckAll()"
                                    style="margin-left:5px;"/>-->
-                            <input name="dsubmit" type="button" value="Delete Selected"
+                            <input name="dsubmit" type="button" value="Borrar seleccion"
                                    class="my_button round blue   text-upper" style="margin-left:5px;"
                                    onclick="return confirmDeleteSubmit()"/>
 <!-- <input type="button" name="Deleteall" value="Delect All Records"
@@ -359,11 +359,11 @@ include_once("init.php");
                                 ?>
                                 <tr>
                                     <th>No</th>
-                                    <th>Supplier Name</th>
-                                    <th>Contact</th>
+                                    <th>Nombre proveedor</th>
+                                    <th>Contacto</th>
                                     <th>Balance</th>
-                                    <th>Edit /Delete</th>
-                                    <th>Select</th>
+                                    <th>Editar/Borar</th>
+                                    <th>Seleccionar</th>
                                 </tr>
 
                                 <?php
@@ -410,9 +410,9 @@ include_once("init.php");
                                     <tr>
                                     <td align='right'style="width:20%"><?php $end=$no+$co1;?>
                                          <?php if($end !=''){?>
-                                            Showing <?php echo $no+1;?> to <?php echo $end;?> of <?php echo $co;?> entries</td><td >&nbsp;</td><td><?php echo $pagination; ?></td>
+                                            Mostrando <?php echo $no+1;?> a <?php echo $end;?> de <?php echo $co;?> entradas</td><td >&nbsp;</td><td><?php echo $pagination; ?></td>
                                  <?php }else{?>
-                                    Showing <?php echo $no;?> to <?php echo $end;?> of <?php echo $co;?> entries</td><td >&nbsp;</td><td><?php echo $pagination; ?></td>
+                                    Mostrando <?php echo $no;?> a <?php echo $end;?> de <?php echo $co;?> entradas</td><td >&nbsp;</td><td><?php echo $pagination; ?></td>
                                       <?php }?>
                                     </tr>
                                

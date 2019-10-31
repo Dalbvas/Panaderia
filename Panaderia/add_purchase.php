f646838290cb40407363de37bb45be99889fe30b
deandu5 <53225134+deandu5@users.noreply.github.com>
2019-10-31 09:17:36 -0600

    Update add_purchase.php

--- Panaderia/add_purchase.php
+++ Panaderia/add_purchase.php
@@ -336,8 +336,8 @@ include_once("init.php");
 
         <!-- FOOTER -->
         <div id="footer">
-           <p>Any Queries email to <a href="mailto:syvoliamary@gmail.com?subject=Stock%20Management%20System">syvoliamary@gmail.com</a>.
-    </p>
+        <p>Cualquier incoveniente comunicarse a: <a href="mailto:it_panaderia@gmail.com?subject=Stock%20Management%20System">it_panaderia@gmail.com</a>.
+            </p>
 
         </div>
         <!-- end footer -->

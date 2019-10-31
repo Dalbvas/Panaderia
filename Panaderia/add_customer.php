4233927ec08eb939a2251be2b571dcdcb640cab6
deandu5 <53225134+deandu5@users.noreply.github.com>
2019-10-31 09:17:32 -0600

    Update add_customer.php

--- Panaderia/add_customer.php
+++ Panaderia/add_customer.php
@@ -213,8 +213,8 @@ include_once("init.php");
 
     <!-- FOOTER -->
     <div id="footer">
-       <p>Cualquier incoveniente comunicarse a:<a href="mailto:it_panaderia@gmail.com?subject=Stock%20Management%20System"> it_panaderia@gmail.com</a>.
-    </p>
+    <p>Cualquier incoveniente comunicarse a: <a href="mailto:it_panaderia@gmail.com?subject=Stock%20Management%20System">it_panaderia@gmail.com</a>.
+            </p>
 
     </div>
     <!-- end footer -->

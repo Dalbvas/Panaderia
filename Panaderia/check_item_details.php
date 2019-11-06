1ad3e79fe6c1f62a8ea0f8484864fedf21cd8382
Dalbvas <52789866+Dalbvas@users.noreply.github.com>
2019-11-05 22:36:59 -0600

    Update check_item_details.php

--- Panaderia/check_item_details.php
+++ Panaderia/check_item_details.php
@@ -4,13 +4,14 @@ include_once("init.php");
 $line = $db->queryUniqueObject("SELECT * FROM stock_details  WHERE stock_name ='" . $_POST['stock_name1'] . "'");
 $cost = $line->company_price;
 $sell = $line->selling_price;
+$category =$line->category;
 $stock_id = $line->stock_id;
 $line = $db->queryUniqueObject("SELECT * FROM stock_avail  WHERE name ='" . $_POST['stock_name1'] . "'");
 $stock = $line->quantity;
 
 if ($line != NULL) {
 
-    $arr = array("cost" => "$cost", "sell" => "$sell", "stock" => "$stock", "guid" => $stock_id);
+    $arr = array("cost" => "$cost", "sell" => "$sell", "stock" => "$stock", "guid" => $stock_id, "category" => "$category");
     echo json_encode($arr);
 
 } else {
